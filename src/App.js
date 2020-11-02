6f3314c053198ea0188149a07424a8c030e7835f
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-02 20:19:09 +0100

    🚧 Add basic Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Figure from "./components/Figure";
+import Header from "./components/Header";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
+  );
 }
 
 export default App;
