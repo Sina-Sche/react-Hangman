01f4531a447e074dc54fb256537ca0c59eada1bd
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-03 09:00:11 +0100

    ✨Add wrongLetter functionality with UseState and UseEffect

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,6 +1,16 @@
 import React from "react";
 
-const Word = () => {
-  return <div className="word" id="word"></div>;
+const Word = ({ selectedWord, correctLetters }) => {
+  return (
+    <div className="word">
+      {selectedWord.split("").map((letter, index) => {
+        return (
+          <span className="letter" key={index}>
+            {correctLetters.includes(letter) ? letter : ""}
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 export default Word;
